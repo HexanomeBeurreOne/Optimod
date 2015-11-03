9c5ae520976efed5fc0d2575d9ce65e8662680d6
V1C0D3R <V1C0D3R@gmail.com>
2015-11-03 08:37:50 +0100

    Add tspalgo pkg in model pkg

---
+++ src/model/tspalgo/IteratorSeq2.java
@@ -0,0 +1,43 @@
+package model.tspalgo;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class IteratorSeq2 implements Iterator<Integer> {
+
+	private Integer[] candidats;
+	private int nbCandidats;
+
+	/**
+	 * Cree un iterateur pour iterer sur l'ensemble des sommets de nonVus qui sont successeurs de sommetCrt dans le graphe g,
+	 * dans l'odre d'apparition dans <code>nonVus</code>
+	 * @param nonVus
+	 * @param sommetCrt
+	 * @param g
+	 */
+	public IteratorSeq2(Collection<Integer> nonVus, int sommetCrt, Graphe g){
+		this.candidats = new Integer[nonVus.size()];
+		Iterator<Integer> it = nonVus.iterator();
+		while (it.hasNext()){
+			Integer s = it.next();
+			if (g.estArc(sommetCrt, s))
+				candidats[nbCandidats++] = s;
+		}
+                Arrays.sort(candidats);
+	}
+	
+	@Override
+	public boolean hasNext() {
+		return nbCandidats > 0;
+	}
+
+	@Override
+	public Integer next() {
+		nbCandidats--;
+		return candidats[nbCandidats];
+	}
+
+	@Override
+	public void remove() {}
+}
