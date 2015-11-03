f620c967272f1cc09d9f274331b719a06bc484ca
Adrien Menella <adrien.menella@gmail.com>
2015-11-03 14:33:03 +0100

    modif FactoryDemandeLivraisons class + modif corresponding tests in main class

--- src/test/main.java
+++ src/test/main.java
@@ -15,7 +15,14 @@ public class main {
 		FactoryDemandeLivraisons factoryDemandeLivraisons = new FactoryDemandeLivraisons();
 		
 		DemandeLivraisons demandeLivraisons = factoryDemandeLivraisons.getDemandeLivraisons("livraison20x20-1.xml", plan);
-		demandeLivraisons.afficheDemandeLivraisons();
+		plan.setDemandeLivraisons(demandeLivraisons);
+		plan.getDemandeLivraisons().afficheDemandeLivraisons();
+		
+		
+		Livraison newLivraison = new Livraison(666, plan.getAdresseById(399), demandeLivraisons.getFenetreLivraison(30600));
+		plan.addLivraison(newLivraison, demandeLivraisons.getFenetreLivraison(30600));
+		
+		plan.getDemandeLivraisons().afficheDemandeLivraisons();
 	}
 
 }
