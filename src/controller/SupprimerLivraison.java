9f89b37609ea4612220eaa8f3887edff4958bd8c
QuentinBay <quentin.bayart45430@gmail.com>
2015-11-03 08:28:53 +0100

    Add classes for controleur package

---
+++ src/controller/SupprimerLivraison.java
@@ -0,0 +1,29 @@
+package controller;
+
+public class SupprimerLivraison implements Commande 
+{
+//------------------------------------------------- ATTRIBUTES	
+
+
+	
+//------------------------------------------------- CONSTRUCTORS
+	
+	
+	
+//------------------------------------------------- METHODS
+	
+	@Override
+	public void execute() 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void unexecute() 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
