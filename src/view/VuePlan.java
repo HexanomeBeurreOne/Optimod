9d2e3cc14cdf1a254834f778a8ecf75784e247de
ccanete <cyril.canete@insa-lyon.fr>
2015-11-03 11:13:28 +0100

    scale with double

--- src/view/VuePlan.java
+++ src/view/VuePlan.java
@@ -3,6 +3,8 @@ package view;
 import model.*;
 
 import java.awt.*;
+import java.awt.geom.Path2D;
+import java.awt.geom.Point2D;
 import java.util.*;
 
 import java.util.List;
@@ -13,13 +15,13 @@ import javax.swing.JPanel;
  */
 public class VuePlan extends JPanel implements Observer {
 
-    private int echelle;
+    private double echelle;
     private int hauteurVue;
     private int largeurVue;
     private Plan plan;
 
 
-    public VuePlan(Plan plan, int echelle) {
+    public VuePlan(Plan plan, double echelle) {
         super();
         //plan.addObserver(this);
         this.plan = plan;
@@ -55,7 +57,7 @@ public class VuePlan extends JPanel implements Observer {
         adresse.afficheAdresse();
         int x = scaleIt(adresse.getCoordX());
         int y = scaleIt(adresse.getCoordY());
-        g2.drawLine(x, y, x, y);
+        g2.fillOval(x - 3, y - 3, 6, 6);
     }
 
     private void drawTroncon(Graphics2D g2, Troncon troncon) {
@@ -70,7 +72,33 @@ public class VuePlan extends JPanel implements Observer {
     }
 
     private int scaleIt(int coordonnee) {
-        return (coordonnee*this.echelle);
+        return (int) Math.round(coordonnee*this.echelle);
+    }
+
+    /**
+     * @param fromPoint end of the arrow
+     * @param rotationDeg rotation angle of line
+     * @param length arrow length
+     * @param wingsAngleDeg wingspan of arrow
+     * @return Path2D arrow shape
+     */
+    public static Path2D createArrowForLine(
+            Point2D fromPoint,
+            double rotationDeg,
+            double length,
+            double wingsAngleDeg) {
+
+        double ax = fromPoint.getX();
+        double ay = fromPoint.getY();
+
+        double radB = Math.toRadians(-rotationDeg + wingsAngleDeg);
+        double radC = Math.toRadians(-rotationDeg - wingsAngleDeg);
+
+        Path2D resultPath = new Path2D.Double();
+        resultPath.moveTo(length * Math.cos(radB) + ax, length * Math.sin(radB) + ay);
+        resultPath.lineTo(ax, ay);
+        resultPath.lineTo(length * Math.cos(radC) + ax, length * Math.sin(radC) + ay);
+        return resultPath;
     }
 
     @Override
