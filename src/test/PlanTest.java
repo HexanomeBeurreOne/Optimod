508cc0afb634e3daddcabb448b1b9fe3ce2d6167
V1C0D3R <V1C0D3R@gmail.com>
2015-11-13 08:56:05 +0100

    Some test lift

--- src/test/PlanTest.java
+++ src/test/PlanTest.java
@@ -69,8 +69,6 @@ public class PlanTest {
 		DemandeLivraisons demandeLivraisons = factoryDemandeLivraisons.getDemandeLivraisons("data/livraison20x20-1.xml", plan);
 		plan.setDemandeLivraisons(demandeLivraisons);
 		
-		assertNull("FAIL 2 : Tournee not null, but it should be null before calcul", plan.getTournee());
-		
 		plan.calculTournee();
 		
 		assertNotNull("FAIL 3 : Tournee null, but it should not be null after calcul", plan.getTournee());
